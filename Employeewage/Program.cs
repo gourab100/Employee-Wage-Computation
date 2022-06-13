a73631ab51285c9450fd08c16ecc97cb743a23a0
gourab <gourab.saheb100@gmail.com>
2022-06-13 14:34:07 +0530

    Add: UC1: Employee attendance is done

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -1,23 +1,23 @@
 ﻿
-
-        int fulldayperhours = 0;
-        int wagesperhours = 20;
+Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+        //int fulldayperhours = 0;
+        //int wagesperhours = 20;
         int present = 1;
-        int daliy_wage = 0;
+        //int daliy_wage = 0;
         Random random = new Random();
         int employee = random.Next(0, 2);
 
         if (employee == present)
         {
-            fulldayperhours = 8;
+          //  fulldayperhours = 8;
             Console.WriteLine("present");
             
         }
         else
         {
-            fulldayperhours = 0;
+            //fulldayperhours = 0;
             Console.WriteLine("absent");
         }
-daliy_wage = fulldayperhours * wagesperhours;
-Console.WriteLine("daliy employee wage :" + daliy_wage);
+//daliy_wage = fulldayperhours * wagesperhours;
+//Console.WriteLine("daliy employee wage :" + daliy_wage);
   
