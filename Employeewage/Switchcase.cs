78f88074af32cf26cdf5e051bda0e2c77dc2fde9
gourab <gourab.saheb100@gmail.com>
2022-06-13 16:10:49 +0530

    Refactor:UC4: Switch case employee

---
+++ Employeewage/Switchcase.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+
+int fulldayperhours = 8;
+int workinghours = 0;
+int wagesperhours = 20;
+const int present = 2;
+const int parttime = 1;
+int daliy_wage = 0;
+Random random = new Random();
+int employee = random.Next(0, 3);
+
+switch (employee)
+{
+    case parttime:
+        workinghours = workinghours + fulldayperhours;
+        Console.WriteLine("parttimer");
+        break;
+    case present:
+        workinghours = (fulldayperhours - workinghours) / 2;
+        Console.WriteLine("present");
+        break;
+    default:
+        Console.WriteLine("Absent");
+        break;
+}
+
+daliy_wage = fulldayperhours * wagesperhours;
+Console.WriteLine("daliy employee wage :" + daliy_wage);
